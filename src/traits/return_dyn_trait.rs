fdd100ea199fa4cc103a460cf361071b9f6a312c
a-givertzman <a.givertzman@icloud.com>
2023-10-16 16:30:17 +0300

    dyn trait

---
+++ src/traits/return_dyn_trait.rs
@@ -0,0 +1,39 @@
+#[derive(Debug)]
+struct Sheep;
+#[derive(Debug)]
+struct Cow;
+
+trait Animal {
+    fn noise(&self) -> &'static str;
+}
+
+impl Animal for Sheep {
+    fn noise(&self) -> &'static str {
+        "Beeeeeeh!"
+    }
+}
+
+impl Animal for Cow {
+    fn noise(&self) -> &'static str {
+        "Mooooooh!"
+    }
+}
+
+fn next(animals: &mut Vec<Box<dyn Animal>>) -> Option<Box<dyn Animal>> {
+    animals.pop()
+}
+
+fn main() {
+    let animals: Vec<Box<dyn Animal>> = vec![
+        Box::new(Sheep{}),
+        Box::new(Sheep{}),
+        Box::new(Cow{}),
+        Box::new(Sheep{}),
+        Box::new(Cow{}),
+        Box::new(Cow{}),
+    ];
+    for animal in animals {
+        println!("animal: {:?}", animal.noise());
+    }
+}
+
