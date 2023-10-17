425074138ccf76c9af0a81b4cb26c3e8348c9d22
a-givertzman <a.givertzman@icloud.com>
2023-10-17 11:03:49 +0300

    itrators

--- src/traits/iterators.rs
+++ src/traits/iterators.rs
@@ -1,6 +1,6 @@
 #![allow(non_snake_case)]
 
-use std::ops::Add;
+use std::{ops::Add, rc::Rc, cell::RefCell};
 
 struct Fibonacci<T> where
     T: Copy + Add {
@@ -23,20 +23,32 @@ impl<T> Iterator for Fibonacci<T> where
 
 fn main() {
     let mut sequence = 0..3;
-    let fibonacciI32 = Fibonacci { curr: 0, next: 1 };
     let fibonacciF32 = Fibonacci { curr: 0.0, next: 1.0 };
+    
+    println!("iterate through 0..3 using for..");
     for i in sequence.clone() {
         println!("squence: {:?}", i);
     }
-    println!("squence: {:?}", sequence.next());
-    println!("squence: {:?}", sequence.next());
-    println!("squence: {:?}", sequence.next());
-    println!("squence: {:?}", sequence.next());
 
-    for i in fibonacciI32.take(10) {
+    println!("iterate through 0..3 using next()..");
+    println!("squence: {:?}", sequence.next());
+    println!("squence: {:?}", sequence.next());
+    println!("squence: {:?}", sequence.next());
+    println!("squence: {:?}", sequence.next());
+    
+    
+    let fibonacciI32 = Fibonacci { curr: 0, next: 1 };
+    println!("first 5 in fibonacci i32..");
+    for i in fibonacciI32.take(5) {
+        println!("fibonacci i32: {:?}", i);
+    }
+    let fibonacciI32 = Fibonacci { curr: 0, next: 1 };
+    println!("next 5 in fibonacci i32..");
+    for i in fibonacciI32.skip(5).take(5) {
         println!("fibonacci i32: {:?}", i);
     }
 
+    println!("ferst 10 in fibonacci f32..");
     for i in fibonacciF32.take(10) {
         println!("fibonacci f32: {:?}", i);
     }
