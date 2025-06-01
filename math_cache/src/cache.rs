0a16ba777ace5ba1ef3bc97cb753d8d14ed24738
Anton Lobanov <a.givertzman@icloud.com>
2025-06-01 16:04:16 +0300

    Math Cache | Cache.get

--- math_cache/src/cache.rs
+++ math_cache/src/cache.rs
@@ -1,7 +1,8 @@
 use indexmap::IndexMap;
 use num::Num;
 use crate::field::Field;
-
+///
+/// 
 pub struct Cache<T> {
     fields: IndexMap<String, Field<T>>,
 }
@@ -18,5 +19,18 @@ impl<T: Num + PartialOrd + Copy> Cache<T> {
             fields,
         }
     }
-
+    ///
+    /// Returns the row, associated with specivied arguments
+    pub fn get(&self, args: IndexMap<String, T>) -> IndexMap<String, T> {
+        let mut result = IndexMap::new();
+        for (key, val) in args {
+            match self.fields.get(&key) {
+                Some(field) => {
+                    let pairs = field.get(val);
+                }
+                None => panic!("Cache.get | Requested key `{key}` - is not found"),
+            }
+        }
+        result
+    }
 }
